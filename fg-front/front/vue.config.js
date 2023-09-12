1b26b671d769eebceeaef449a26775e4e4ffc194
inism <nism@aliyun.com>
2023-09-12 10:58:06 +0800

    add svg icon and modify the front package function

--- fg-front/front/vue.config.js
+++ fg-front/front/vue.config.js
@@ -1,17 +1,93 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const CompressionPlugin = require('compression-webpack-plugin');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-process.env.VUE_APP_MA_VERSION = require('./package.json').version
+process.env.VUE_APP_MA_VERSION = require('./package.json').version;
+const path = require('path');
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
 
 module.exports = defineConfig({
-  // 在npm run build 或 yarn build 时 ，生成文件的目录名称（要和baseUrl的生产环境路径一致）（默认dist）
-  outputDir: 'dist',
-  // 用于放置生成的静态资源 (js、css、img、fonts) 的；（项目打包之后，静态资源会放在这个文件夹下）
   assetsDir: 'static',
-  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
   productionSourceMap: false,
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new MonacoWebpackPlugin()]
-  }
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    },
+    plugins: [
+      new CompressionPlugin({
+        test: /\.(js|css|html|svg)?$/i, // 压缩文件格式
+        filename: '[path][base].gz',    // 压缩后的文件名
+        algorithm: 'gzip',              // 使用gzip压缩
+        minRatio: 0.8,                  // 压缩率小于1才会压缩
+        threshold: 10240,               // 压缩率小于1才会压缩
+      })
+    ],
+  },
+  chainWebpack: config => {
+    // set svg-sprite-loader
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/assets/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/assets/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+    config
+      .when(true,
+        config => {
+          config
+            .plugin('ScriptExtHtmlWebpackPlugin')
+            .after('html')
+            .use('script-ext-html-webpack-plugin', [{
+              inline: /runtime\..*\.js$/
+            }])
+            .end()
+          config
+            .optimization.splitChunks({
+              chunks: 'all',
+              cacheGroups: {
+                libs: {
+                  name: 'chunk-libs',
+                  test: /[\\/]node_modules[\\/]/,
+                  priority: 10,
+                  chunks: 'initial'
+                },
+                elementUI: {
+                  name: 'chunk-element-ui',
+                  priority: 20,
+                  test: /[\\/]node_modules[\\/]_?element-ui(.*)/
+                },
+                monacoEditor: {
+                  name: 'chunk-monaco-editor',
+                  priority: 20,
+                  test: /[\\/]node_modules[\\/]_?monaco-editor(.*)/
+                },
+                commons: {
+                  name: 'chunk-commons',
+                  test: resolve('src/components'),
+                  minChunks: 3,
+                  priority: 5,
+                  reuseExistingChunk: true
+                }
+              }
+            })
+        }
+      )
+  },
 })
 
