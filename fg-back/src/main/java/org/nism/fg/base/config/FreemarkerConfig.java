d2058783c5295152ed0b9f8b24a22798e73b4dd2
inism <nism@aliyun.com>
2024-03-11 15:30:02 +0800

    import hutool strUtil all

--- fg-back/src/main/java/org/nism/fg/base/config/FreemarkerConfig.java
+++ fg-back/src/main/java/org/nism/fg/base/config/FreemarkerConfig.java
@@ -1,18 +1,15 @@
 package org.nism.fg.base.config;
 
 import cn.hutool.core.io.FileUtil;
-import freemarker.template.Configuration;
-import freemarker.template.TemplateExceptionHandler;
-import freemarker.template.TemplateModel;
-import freemarker.template.TemplateModelException;
-import org.nism.fg.base.freemarker.*;
+import freemarker.ext.beans.BeansWrapperBuilder;
+import freemarker.template.*;
 import org.nism.fg.base.utils.SystemUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
+
+import static freemarker.template.Configuration.VERSION_2_3_31;
 
 /**
  * 模板引擎配置
@@ -23,21 +20,16 @@ import java.util.Map;
 @org.springframework.context.annotation.Configuration
 public class FreemarkerConfig {
 
+    private final static TemplateHashModel staticModels = new BeansWrapperBuilder(VERSION_2_3_31).build().getStaticModels();
+
     @Bean
     @Primary
     public Configuration cfg() throws IOException, TemplateModelException {
-        Configuration cfg = new Configuration(freemarker.template.Configuration.VERSION_2_3_31);
+        Configuration cfg = new Configuration(VERSION_2_3_31);
         cfg.setDirectoryForTemplateLoading(FileUtil.mkdir(SystemUtils.getTemplateDir()));
         cfg.setDefaultEncoding("UTF-8");
         cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
-
-        Map<String, TemplateModel> strUtil = new HashMap<>();
-        strUtil.put("toUnderline", new ToUnderlineCaseMethodModel());
-        strUtil.put("toCame", new ToCameCaseMethodModel());
-        strUtil.put("toPascal", new ToPascalCaseCaseMethodModel());
-        strUtil.put("lowerFirst", new LowerFirstMethodModel());
-        strUtil.put("upperFirst", new UpperFirstMethodModel());
-
+        TemplateModel strUtil = staticModels.get("cn.hutool.core.util.StrUtil");
         cfg.setSharedVariable("strUtil", strUtil);
         return cfg;
     }
